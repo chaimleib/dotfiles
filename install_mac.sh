7ee09fc5d49e335fe12012e1ffdd6cd6ed2b62b6
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-07-28 11:29:32 -0500

    made installer symlink Razer profiles
    Former-commit-id: 2a6416620cadcde00ab550f36cd5a8fba488aa78

--- install_mac.sh
+++ install_mac.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
-this_dir="$(basedir "$1")"
 
+function abspath() {
+    pushd "$1" >/dev/null
+    echo "$PWD"
+    popd >/dev/null
+}
 
+this_dir="$(abspath "$(dirname "$0")")"
+
+function macroot() {
+    local macroot="$this_dir/macroot"
+    [[ -d "$macroot" ]] || return
+    
+    local d="/Library/Application Support/Razer"
+    _lndir "$macroot$d" "$d"
+}
+
+function _rmdir() {
+    [ -d "$1" ] && sudo rm -rf "$1"
+}
+
+function _lndir() {
+    _rmdir "$2"
+    echo "ln -s \"$1\" \"$2\""
+    sudo ln -s "$1" "$2"
+}
+
+function _rmfile() {
+    [[ -e "$1" ]] && sudo rm "$1"
+}
+
+function _lnfile() {
+    _rmfile "$2"
+    echo "ln -s \"$1\" \"$2\""
+    sudo ln -s "$1" "$2"
+}
+
+macroot
 
