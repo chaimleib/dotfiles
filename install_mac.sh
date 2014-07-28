3f67532388eb0aaf8f62e001c879c0340e350633
Chaim-Leib Halbert <chaimleib.halbert@scorm.com>
2014-07-28 11:44:06 -0500

    added ControllerMate profile
    Former-commit-id: 901790b7e2a78ff661043ff32f8f819ae635d30d

--- install_mac.sh
+++ install_mac.sh
@@ -8,33 +8,66 @@ function abspath() {
 
 this_dir="$(abspath "$(dirname "$0")")"
 
+function install_mac() {
+    $this_dir/osx.sh
+    macroot
+    machome
+}
+
 function macroot() {
     local macroot="$this_dir/macroot"
     [[ -d "$macroot" ]] || return
     
     local d="/Library/Application Support/Razer"
-    _lndir "$macroot$d" "$d"
+    _slndir "$macroot$d" "$d"
+}
+
+function machome() {
+    local machome="$this_dir/machome"
+    [[ -d "$machome" ]] || return
+    
+    local d="/Library/Application Support/ControllerMate"
+    _lndir "$machome$d" "$d"
 }
 
 function _rmdir() {
-    [ -d "$1" ] && sudo rm -rf "$1"
+    [ -d "$1" ] && rm -rf "$1"
 }
 
 function _lndir() {
     _rmdir "$2"
     echo "ln -s \"$1\" \"$2\""
-    sudo ln -s "$1" "$2"
+    ln -s "$1" "$2"
 }
 
 function _rmfile() {
-    [[ -e "$1" ]] && sudo rm "$1"
+    [[ -e "$1" ]] && rm "$1"
 }
 
 function _lnfile() {
     _rmfile "$2"
     echo "ln -s \"$1\" \"$2\""
+    ln -s "$1" "$2"
+}
+function _srmdir() {
+    [ -d "$1" ] && sudo rm -rf "$1"
+}
+
+function _slndir() {
+    _srmdir "$2"
+    echo "ln -s \"$1\" \"$2\""
     sudo ln -s "$1" "$2"
 }
 
-macroot
+function _srmfile() {
+    [[ -e "$1" ]] && sudo rm "$1"
+}
+
+function _slnfile() {
+    _srmfile "$2"
+    echo "ln -s \"$1\" \"$2\""
+    sudo ln -s "$1" "$2"
+}
+
+install_mac
 
