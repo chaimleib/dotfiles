0a7b05444385370b8e610869150996ccc99975bb
Chaim-Leib Halbert <chaimleib.halbert@scorm.com>
2014-08-08 10:37:31 -0500

    split off common functions to install_common.sh
    Former-commit-id: 77d77a170b0d69a713359dd2fbc6f028bc5dcf67

---
+++ install_common.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+
+function abspath() {
+    if [[ -d "$1" ]]; then
+        pushd "$1" >/dev/null
+        echo "$PWD"
+        popd >/dev/null
+    else
+        local file=$(basename  "$1")
+        local dir=$(dirname "$1")
+        pushd "$dir" >/dev/null
+        echo "$PWD/$file"
+        popd >/dev/null
+    fi 
+}
+
+# this_dir="$(abspath "$(dirname "$0")")"
+
+function _slnargs() {
+    local this_dir="$PWD"
+    for item in "$@"; do
+        if [[ -d "$this_dir/$item" ]]; then
+            _slndir "$this_dir/$item" "$HOME/$item"
+        elif [[ -f "$this_dir/$item" ]]; then
+            _slnfile "$this_dir/$item" "$HOME/$item"
+        else
+            echo "Could not link '$file'"
+        fi
+    done
+}
+
+function _lnargs() {
+    local this_dir="$PWD"
+    for item in "$@"; do
+        if [[ -d "$this_dir/$item" ]]; then
+            _lndir "$this_dir/$item" "$HOME/$item"
+        elif [[ -f "$this_dir/$item" ]]; then
+            _lnfile "$this_dir/$item" "$HOME/$item"
+        else
+            echo "Could not link '$file'"
+        fi
+    done
+}
+
+function _rmdir() {
+    [ -d "$1" ] && mv "$1{,.old}"
+}
+
+function _lndir() {
+    _rmdir "$2"
+    echo "ln -s \"$1\" \"$2\""
+    ln -s "$1" "$2"
+}
+
+function _rmfile() {
+    [[ -e "$1" ]] && mv "$1{,.old}"
+}
+
+function _lnfile() {
+    _rmfile "$2"
+    echo "ln -s \"$1\" \"$2\""
+    ln -s "$1" "$2"
+}
+
+function _srmdir() {
+    [ -d "$1" ] && sudo mv "$1{,.old}"
+}
+
+function _slndir() {
+    _srmdir "$2"
+    echo "ln -s \"$1\" \"$2\""
+    sudo ln -s "$1" "$2"
+}
+
+function _srmfile() {
+    [[ -e "$1" ]] && sudo mv "$1{,.old}"
+}
+
+function _slnfile() {
+    _srmfile "$2"
+    echo "ln -s \"$1\" \"$2\""
+    sudo ln -s "$1" "$2"
+}
+
--- install_mac.sh
+++ install_mac.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-function abspath() {
-    pushd "$1" >/dev/null
-    echo "$PWD"
-    popd >/dev/null
-}
+. "$this_dir/install_common.sh"
 
 this_dir="$(abspath "$(dirname "$0")")"
 
@@ -12,61 +8,37 @@ function install_mac() {
     $this_dir/osx.sh
     macroot
     machome
+    pushd "$HOME" >/dev/null
+    . .bashrc
+    popd >/dev/null
 }
 
 function macroot() {
-    local macroot="$this_dir/macroot"
-    [[ -d "$macroot" ]] || return
+    pushd "$this_dir/macroot" >/dev/null
     
-    local d="/Library/Application Support/Razer"
-    _slndir "$macroot$d" "$d"
+    _slnargs Library/Application\ Support/Razer
+    
+    popd >/dev/null
 }
 
 function machome() {
-    local machome="$this_dir/machome"
-    [[ -d "$machome" ]] || return
+    pushd "$this_dir/machome"
     
-    local d="/Library/Application Support/ControllerMate"
-    _lndir "$machome$d" "$HOME$d"
-}
-
-function _rmdir() {
-    [ -d "$1" ] && rm -rf "$1"
-}
-
-function _lndir() {
-    _rmdir "$2"
-    echo "ln -s \"$1\" \"$2\""
-    ln -s "$1" "$2"
-}
-
-function _rmfile() {
-    [[ -e "$1" ]] && rm "$1"
-}
-
-function _lnfile() {
-    _rmfile "$2"
-    echo "ln -s \"$1\" \"$2\""
-    ln -s "$1" "$2"
-}
-function _srmdir() {
-    [ -d "$1" ] && sudo rm -rf "$1"
-}
-
-function _slndir() {
-    _srmdir "$2"
-    echo "ln -s \"$1\" \"$2\""
-    sudo ln -s "$1" "$2"
-}
-
-function _srmfile() {
-    [[ -e "$1" ]] && sudo rm "$1"
-}
-
-function _slnfile() {
-    _srmfile "$2"
-    echo "ln -s \"$1\" \"$2\""
-    sudo ln -s "$1" "$2"
+    _lnargs \
+        "/Library/Application Support/ControllerMate" \
+        .bashrc \
+        .bashrc.d \
+        .bash_completion \
+        .bash_completion.d \
+        .vimrc \
+        .vim \
+        local \
+        .hushlogin \
+        .ee.bcrc \
+        .phy.bcrc \
+        .profile
+    
+    popd >/dev/null
 }
 
 install_mac
