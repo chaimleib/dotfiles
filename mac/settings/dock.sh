92d513c2d2d9583be17799201d2a601bea458b70
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-08-18 18:14:23 -0500

    more dock settings; moved dockItems.sh to dock.sh
    Former-commit-id: 7b7238f7b1742b3a4ab8baf0988da1ab3c9be096

--- mac/settings/dock.sh
+++ mac/settings/dock.sh
@@ -4,6 +4,7 @@ this_dir="$(dirname "$0")"
 
 . "$this_dir"/utils/addDockItem.sh
 
+echo "## Dock ##"
 echo Clear the dock\'s apps
 defaults write com.apple.dock persistent-apps -array
 
@@ -28,5 +29,10 @@ done
 echo Dim hidden apps in the dock
 defaults write com.apple.dock showhidden -bool true
 
+echo "Pin dock to left edge"
+defaults write com.apple.dock orientation -string "left"
+
+echo "Autohide dock"
+defaults write com.apple.dock autohide -bool true
+
 killall Dock
-    
