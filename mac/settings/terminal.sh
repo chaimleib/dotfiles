90aa250f51ec766f868ce895156d03cd9e5711c5
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-08-18 17:20:39 -0500

    close Terminal windows when shell exits
    Former-commit-id: 3abc7a8b7ad5152a7d0b4e093db4b2a90dd1207f

--- mac/settings/terminal.sh
+++ mac/settings/terminal.sh
@@ -2,6 +2,12 @@
 
 echo
 echo "## Terminal ##"
+
+echo "Close window on shell exit"
+# TODO: Get Terminal to reload its prefs and not overwrite this
+ /usr/libexec/PlistBuddy -c 'Set "Window Settings:Pro:shellExitAction" 0' ~/Library/Preferences/com.apple.Terminal.plist
+ 
 echo "Set default theme to Pro in Terminal"
 defaults write com.apple.Terminal "Default Window Settings" -string Pro
 defaults write com.apple.Terminal "Startup Window Settings" -string Pro
+
