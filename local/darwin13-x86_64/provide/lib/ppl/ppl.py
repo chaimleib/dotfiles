d154b0b2ffc20de66f47c7c02f8d0eff6a32f5a7
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-08-18 18:41:42 -0500

    made ppl work with python 2; made ppl compatible with binary plists
    Former-commit-id: d6df1cccaf4037e0da753dff6279b9f099fc55b8

--- local/darwin13-x86_64/provide/lib/ppl/ppl.py
+++ local/darwin13-x86_64/provide/lib/ppl/ppl.py
@@ -14,12 +14,20 @@ def parse_args(args):
     result = {}
     if len(args) == 2:
         result['file'] = args[1]
+    elif len(args) == 1:
+        result['file'] = sys.stdin
     return result
 
 def show_file(file, opts=None):
     if opts is None:
         opts = {}
     plist = pll.readPlist(file)
+    pprint(plist)
+
+def show_string(str, opts=None):
+    if opts is None:
+        opts = {}
+    plist = pll.readPlistFromString(str)
     pprint(plist)
 
 main(sys.argv)
