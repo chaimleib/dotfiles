5ed15faa3744cba20ac4ef8263b8a5ab8f2058a0
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-08-18 13:13:25 -0500

    added osVersion.sh
    Former-commit-id: 5e006b90447bc08df66ce8d2ea2251dbee2ceaf2

--- mac/installers/utils/safariAgent.sh
+++ mac/installers/utils/safariAgent.sh
@@ -5,12 +5,11 @@ if [[ ! -e /Applications/Safari.app ]]; then
     exit 1
 fi
 
+this_dir="$(dirname "$0")"
 infoPlist="/Applications/Safari.app/Contents/Info.plist"
 shortVer=$(defaults read "$infoPlist" CFBundleShortVersionString)
 longVer=$(defaults read "$infoPlist" CFBundleVersion)
-osVer="$(system_profiler SPSoftwareDataType Software |
-    grep 'System Version' | 
-    grep -o '[0-9]\+\.[0-9\.]\+' |
+osVer="$("$this_dir"/osVersion.sh |
     sed 's/\./_/g')" # Gives 10_9_4
 
 echo "Mozilla/5.0 (Macintosh; Intel Mac OS X $osVer) AppleWebKit/${longVer#?} (KHTML, like Gecko) Version/$shortVer Safari/${longVer#?}"
