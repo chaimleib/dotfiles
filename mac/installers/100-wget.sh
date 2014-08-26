2838c9ea9e5a8e5e0646189c757c231404e45987
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-08-26 11:10:58 -0700

    redid installer for wget
    Former-commit-id: 56bc49c09c1558c35138b894941ba15ea7f0e8dd

--- mac/installers/100-wget.sh
+++ mac/installers/100-wget.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-printf "%-30s" "Installing wget..."
-if [[ "$(which wget)" == "/usr/local/bin/wget" ]]; then
-    version="$(wget --version 2>&1 | grep -o '[0-9]\+\.[0-9\.]\+' | head -n1)"
+cmdName=wget
+
+printf "%-30s" "Installing $ack..."
+if [[ "$(which "$cmdName")" == "/usr/local/bin/$cmdName" ]]; then
+    version="$("$cmdName" --version 2>&1 | grep -o '[0-9]\+\.[0-9\.]\+' | head -n1)"
     echo "Already installed $version"
 else
-    brew install wget
-    version="$(wget --version 2>&1 | grep -o '[0-9]\+\.[0-9\.]\+' | head -n1)"
+    brew install "$cmdName"
+    version="$("$cmdName" --version 2>&1 | grep -o '[0-9]\+\.[0-9\.]\+' | head -n1)"
     echo "Installed $version"
 fi
 
