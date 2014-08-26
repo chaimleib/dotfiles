8f1fb82b6654d34d3ce9fbb7004128790ed078c2
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-08-26 11:08:40 -0700

    added mac installer for ack
    Former-commit-id: 54f7aade2b093aa854ac6f8558e6cde2ecbe9463

---
+++ mac/installers/102-ack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cmdName=ack
+
+printf "%-30s" "Installing $ack..."
+if [[ "$(which "$cmdName")" == "/usr/local/bin/$cmdName" ]]; then
+    version="$("$cmdName" --version 2>&1 | grep -o '[0-9]\+\.[0-9\.]\+' | head -n1)"
+    echo "Already installed $version"
+else
+    brew install "$cmdName"
+    version="$("$cmdName" --version 2>&1 | grep -o '[0-9]\+\.[0-9\.]\+' | head -n1)"
+    echo "Installed $version"
+fi
+
