cb7a032ef2ea36549e8943123bdd17f06fbce217
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-10-20 00:02:26 -0700

    fixed url for homebrew installer
    Former-commit-id: abb474ccd18761723b23fef0ddca2f2b922f54b9

--- mac/installers/001-homebrew.sh
+++ mac/installers/001-homebrew.sh
@@ -5,7 +5,7 @@ if brew --version 1>/dev/null 2>/dev/null; then
     version="$(brew --version | grep -o '[0-9]\+\.[0-9\.]\+')"
     echo "Already installed $version"
 else
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    ruby -e "$(curl -fsSL https://raw.githusercontent.com/Homebrew/install/master/install)"
     if brew doctor; then
         brew update
         brew upgrade
