bac478f40e4e404be58b46349fbd494b47aa8d0b
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-10-23 13:58:31 -0700

    ssh.sh exits early properly if settings present
    Former-commit-id: f640cf3562e0f7339a6ef740c2eda392705968a4

--- common/settings/ssh.sh
+++ common/settings/ssh.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-if [[ ! -d "$HOME"/.ssh ]]; then
-    mkdir "$HOME"/.ssh
-fi
+function setup_ids() {
+    if [[ ! -d "$HOME"/.ssh ]]; then
+        mkdir "$HOME"/.ssh
+    fi
 
-if [[ -e "$HOME"/.ssh/id_rsa && -e "$HOME"/.ssh/id_rsa.pub ]]; then
-    return
-fi
+    if [[ -e "$HOME"/.ssh/id_rsa && -e "$HOME"/.ssh/id_rsa.pub ]]; then
+        return
+    fi
 
-ssh-keygen -t rsa -C "chaim.leib.halbert@gmail.com"
-eval "$(ssh-agent -s)"
-ssh-add "$HOME"/.ssh/id_rsa
+    ssh-keygen -t rsa -C "chaim.leib.halbert@gmail.com"
+    eval "$(ssh-agent -s)"
+    ssh-add "$HOME"/.ssh/id_rsa
+}
 
+setup_ids
