6cfc22416cfa56f23b42b4879061f70cf7812555
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-12-07 02:22:15 -0800

    removed annoying space at beginning of git prompt
    Former-commit-id: 43cbe979ec6818071ad7074887f1a25de27aeca9

--- .bashrc.d/84-prompt.sh
+++ .bashrc.d/84-prompt.sh
@@ -5,7 +5,7 @@ export GIT_PS1_SHOWUPSTREAM=verbose
 #export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWCOLORHINTS=1
 
-export PROMPT_COMMAND='__git_ps1 "\u@\[\e[31m\]`hostname`\[\e[0m\]:\[\e[32;1m\]\w\[\e[0m\]\n" "\$ "'
+export PROMPT_COMMAND='__git_ps1 "\u@\[\e[31m\]`hostname`\[\e[0m\]:\[\e[32;1m\]\w\[\e[0m\]\n" "\$ " "(%s) "'
 export HISTCONTROL=ignoredups
 shopt -s histappend;
 
