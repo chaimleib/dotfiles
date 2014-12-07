2a1763de2b477a061458bba4da7cfa34ea88ff8f
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-12-07 02:14:49 -0800

    add: git prompt
    Former-commit-id: 7cc9cd19cffc217f89879e508c244031aa0e2e5e

---
+++ .bashrc.d/84-prompt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWSTASHSTATE=1
+export GIT_PS1_SHOWUPSTREAM=verbose
+#export GIT_PS1_SHOWUNTRACKEDFILES=1
+export GIT_PS1_SHOWCOLORHINTS=1
+
+export PROMPT_COMMAND='__git_ps1 "\u@\[\e[31m\]`hostname`\[\e[0m\]:\[\e[32;1m\]\w\[\e[0m\]\n" "\$ "'
+export HISTCONTROL=ignoredups
+shopt -s histappend;
+
