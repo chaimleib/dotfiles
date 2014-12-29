cd125e225eee91606ed3f9b691671582114082a9
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-12-28 22:29:25 -0600

    add: local perl dir support
    Former-commit-id: 9b510643836f365f762fd7fe1cbae263249c8dfd

--- .bashrc.d/70-env.sh
+++ .bashrc.d/70-env.sh
@@ -3,4 +3,12 @@
 export EDITOR="`which vim`"
 export VISUAL="$EDITOR"
 
+# Python
 [[ -f "$HOME/.pythonrc.py" ]] && export PYTHONSTARTUP="$HOME/.pythonrc.py"
+
+# Perl
+if [[ -d "${HOME}/.perl5" ]]; then
+    export PERL_MB_OPT="--install_base \"${HOME}/.perl5\""
+    export PERL_MM_OPT="INSTALL_BASE=${HOME}/.perl5"
+    export PERL5LIB="${HOME}/.perl5/lib/perl5"
+fi
