66df6d7da4c813aec36180d74ed7edb47910a167
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-02-06 13:54:40 -0800

    make homebrew cask link apps to /Applications
    Former-commit-id: d919a7ad111ddb150c93e3d294309d40ec674bf8

--- .bashrc.d/70-env.sh
+++ .bashrc.d/70-env.sh
@@ -12,3 +12,9 @@ if [[ -d "${HOME}/.perl5" ]]; then
     export PERL_MM_OPT="INSTALL_BASE=${HOME}/.perl5"
     export PERL5LIB="${HOME}/.perl5/lib/perl5"
 fi
+
+# Homebrew Cask
+if [[ "$OSTYPE" == darwin* ]]; then
+    export HOMEBREW_CASK_OPTS='--appdir=/Applications'
+fi
+
