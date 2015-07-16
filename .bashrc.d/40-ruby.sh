1bbd2cac4cb9dbe1b490e49a4b880fbcd63b11b6
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-16 11:19:29 -0700

    ruby: rp=Rails project path, rr=rails runner
    Former-commit-id: e3cfa96ff5307093fb11f6d488429159348ee454

--- .bashrc.d/40-ruby.sh
+++ .bashrc.d/40-ruby.sh
@@ -7,3 +7,10 @@ fi
 # Load RVM into a shell session *as a function*
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
+# Rails
+alias rp='bundle exec rails runner "puts Rails.root"'
+function rr() {
+    fp="$(relpath "$1" "$(rp)")"
+    bundle exec rails runner "eval(File.read '$fp')"
+}
+
