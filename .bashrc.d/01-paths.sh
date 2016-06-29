678bf8aeea6a594d525a4de516080cd7a0d1d893
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2016-06-29 12:03:35 -0700

    split: 02-osdeppaths.sh from 01-paths.sh
    Former-commit-id: d703d43865fa99e56a2e1b7a476895764546991c

--- .bashrc.d/01-paths.sh
+++ .bashrc.d/01-paths.sh
@@ -67,31 +67,3 @@ if [[ -d /usr/local/Cellar/ruby ]]; then
 fi
 appendPath "$HOME/.rvm/bin"   # Add RVM to PATH for scripting
 
-# OS-specific
-
-function _addOPPathTree() {
-    bin_dirs=(
-        bin
-        sbin
-        )
-    if [[ -d "$1" ]]; then
-        tree=override
-        [[ -d "$1/$tree" ]] &&
-            for bin in ${bin_dirs[*]}; do
-                prependPath "$1/$tree/$bin"
-            done
-        
-        tree=provide
-        [[ -d "$1/$tree" ]] &&
-            for bin in ${bin_dirs[*]}; do
-                appendPath "$1/$tree/$bin"
-            done
-    fi
-}
-
-if [[ "$OSTYPE" == darwin* ]]; then
-    _addOPPathTree "${HOME}/local/darwin-`uname -m`"
-fi
-_addOPPathTree "${HOME}/local/$OSTYPE-`uname -m`"
-_addOPPathTree "${HOME}/local/all"
-
