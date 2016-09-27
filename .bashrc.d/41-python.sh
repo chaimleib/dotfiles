d0073825a1d8438885ea00cfd6d628e612a3af50
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2016-09-27 14:39:43 -0700

    init virtualenvwrapper for python if present

--- .bashrc.d/41-python.sh
+++ .bashrc.d/41-python.sh
@@ -6,3 +6,5 @@
 # disabled, since it pushes the rvm path from first position and causes warnings.
 #if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 
+[ -f /usr/local/bin/virtualenvwrapper.sh ] && source /usr/local/bin/virtualenvwrapper.sh
+
