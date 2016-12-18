8c70103811b02c04f41f3b4c983a929e9a69ea50
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2016-12-18 14:58:41 -0800

    rm dpkg-divert for ack, no longer needed

--- linux/installers/102-ack.sh
+++ linux/installers/102-ack.sh
@@ -4,7 +4,6 @@ function do_install() {
     [[ -z "$INSTALL" ]] && echo "INSTALL not set" && return 1
     
     $INSTALL ack-grep
-    sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
 }
 
 do_install
