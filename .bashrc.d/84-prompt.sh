0db9e848d64d4496b9b3dde36f60f93b73c2c82a
Chaim Halbert <chalbert@evernote.com>
2018-02-09 13:39:29 -0800

    prompt progress

--- .bashrc.d/84-prompt.sh
+++ .bashrc.d/84-prompt.sh
@@ -12,6 +12,31 @@ if [[ "$0" == *bash ]]; then
   shopt -s histappend;
   export PROMPT_COMMAND='__git_ps1 "$([[ "$?" -eq 0 ]] && echo "\[\e[32;1m\]" || echo "\[\e[31;1m\]")➜ \e[0m\]\u@\[\e[31m\]`hostname`\[\e[0m\]:\[\e[32;1m\]\w\[\e[0m\]\n" "\$ " "(%s) "'
 elif [[ -n "$ZSH_NAME" ]]; then
-  export PS1='$USER@%{$fg[red]%}$HOST%{$reset_color%}:%{$fg_bold[green]%}%c%{$reset_color%}'$'\n''$(git_prompt_info)${ret_status}%{$reset_color%}'
+  # http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html
+  # %n - $USERNAME
+  # %M - full machine hostname
+  # %m - hostname up to the first '.'. Int after % for # components
+  # %# - # for root, % otherwise
+  # %c - trailing component of cwd. Int after % for # components. Tilde
+  #      contraction unless %C. Deprecated, equiv to %1~ and %1/.
+  # %~ - tilde-contracted cwd. Int after % for # components.
+  # %/ - cwd. Int after % for # components.
+  # %{...%} - Include escape sequences; string inside should not change cursor
+  #           position, except if %G included. Nesting allowed.
+  # %G - inside %{...%}, increase string width by 1, or by # after %. Affects
+  #      prompt truncation, when in use.
+  PROMPT=''
+  PROMPT+='${ret_status}%{$reset_color%}'
+  PROMPT+='%n'
+  PROMPT+=@
+  PROMPT+='%{$fg[red]%}%M%{$reset_color%}'
+  PROMPT+=:
+  PROMPT+='%{$fg_bold[green]%}%~%{$reset_color%}'
+  PROMPT+=$'\n'
+  export ZSH_THEME_GIT_PROMPT_PREFIX=''
+  export ZSH_THEME_GIT_PROMPT_SUFFIX=''
+  PROMPT+='$(git_prompt_info)%{$reset_color%} '
+  PROMPT+='%# '
+  export PROMPT
 fi
 
