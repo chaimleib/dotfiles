de866a4fb81afa4308cdd188a93e1fd97cf4049c
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2018-02-09 10:15:10 -0800

    zsh prompt fix

--- .bashrc.d/84-prompt.sh
+++ .bashrc.d/84-prompt.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 [[ -z "$PS1" ]] && return
-
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWUPSTREAM=verbose
@@ -12,7 +11,7 @@ export HISTCONTROL=ignoredups
 if [[ "$0" == *bash ]]; then
   shopt -s histappend;
   export PROMPT_COMMAND='__git_ps1 "$([[ "$?" -eq 0 ]] && echo "\[\e[32;1m\]" || echo "\[\e[31;1m\]")➜ \e[0m\]\u@\[\e[31m\]`hostname`\[\e[0m\]:\[\e[32;1m\]\w\[\e[0m\]\n" "\$ " "(%s) "'
-elif [[ "$0" == *zsh ]]; then
-  export PROMPT='${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)'
+elif [[ -n "$ZSH_NAME" ]]; then
+  export PS1='$USER@%{$fg[red]%}$HOST%{$reset_color%}:%{$fg_bold[green]%}%c%{$reset_color%}'$'\n''$(git_prompt_info)${ret_status}%{$reset_color%}'
 fi
 
