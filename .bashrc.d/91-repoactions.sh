05f9af90c95ac9bfc65741bff1d367c0761168a1
Chaim Halbert <chalbert@evernote.com>
2018-02-12 15:25:59 -0800

    enable repoactions for zsh

--- .bashrc.d/91-repoactions.sh
+++ .bashrc.d/91-repoactions.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-if have repoactions; then
+have repoactions || return
+if [[ "$0" == *bash ]]; then
     if [[ -n "$PROMPT_COMMAND" ]] && [[ "$PROMPT_COMMAND" != *';' ]]; then
         PROMPT_COMMAND="$PROMPT_COMMAND;"
     fi
     PROMPT_COMMAND="$PROMPT_COMMAND"'eval "$(repoactions -e)"'
     export PROMPT_COMMAND
+elif [[ -n "$ZSH_NAME" ]]; then
+    function repoactions_func() {
+        eval "$(repoactions -e)"
+    }
+    precmd_functions+=( repoactions_func )
 fi
 
