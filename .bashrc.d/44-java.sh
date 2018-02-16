a5cd2f58da2ea14018739ebde55d1692ff788c34
Chaim Halbert <chalbert@evernote.com>
2018-02-15 17:20:41 -0800

    add java.sh

---
+++ .bashrc.d/44-java.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -d "/Library/Java/Home" ]; then # Mac
+    JAVA_HOME="/Library/Java/Home"
+elif [ -d "/usr/local/java-current" ]; then # Linux
+    JAVA_HOME="/usr/local/java-current"
+    prependPath "$JAVA_HOME"/bin
+fi
+export JAVA_HOME
+
