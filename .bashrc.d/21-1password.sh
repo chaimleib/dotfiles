4f0dbb9b547f5d1650c6484c839a6980e10a5889
Chaim Halbert <chalbert@evernote.com>
2018-02-21 18:37:44 -0800

    add signin for opp

--- .bashrc.d/21-1password.sh
+++ .bashrc.d/21-1password.sh
@@ -3,6 +3,8 @@
 have op && have jq || return
 function opp() {
     local item="$1"
+    op get account &>/dev/null || eval $(op signin my) | grep -v '^#' >&2 || return 1
     op get item "$1" | jq '.details.fields[]|select(.designation=="password").value' -r
+    return $?
 }
 
