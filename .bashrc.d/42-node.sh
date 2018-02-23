2bd05d4f554ee2434c9f13ec9b563929eb80dca9
Chaim Halbert <chalbert@evernote.com>
2018-02-22 16:48:40 -0800

    fix node.sh to run even in interactive subshells

--- .bashrc.d/42-node.sh
+++ .bashrc.d/42-node.sh
@@ -2,18 +2,20 @@
 
 # Initialize nvm shell function
 export NVM_DIR="$HOME/.nvm"
-[ -s $NVM_DIR/nvm.sh ] && \. "$NVM_DIR/nvm.sh"
+[[ -s $NVM_DIR/nvm.sh ]] && \. "$NVM_DIR/nvm.sh"
 
 # Detect node version from noderc
 function noderc_update() {
-    if [ "$PWD" != "$PREV_PWD" ]; then
+    if [[ "$PWD" != "$PREV_PWD" ]]; then
         PREV_PWD="$PWD"
-        if [ -e ".nvmrc" ]; then
-            nvm install;
+        if [[ -e ".nvmrc" ]]; then
+            nvm install
         fi
     fi
 }
 
+[[ "$0" == *bash ]] && export -f noderc_update
+
 # Run after every user command
-export PROMPT_COMMAND="noderc_update; $PROMPT_COMMAND"
+PROMPT_COMMAND="$PROMPT_COMMAND; noderc_update"
 
