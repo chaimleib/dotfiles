b940c3c04f59f041f17f56d0128faa6d1b9f4971
Chaim Halbert <chalbert@evernote.com>
2018-03-16 12:43:22 -0700

    add opmy, login check to opp

--- .bashrc.d/21-1password.sh
+++ .bashrc.d/21-1password.sh
@@ -3,8 +3,14 @@
 have op && have jq || return
 function opp() {
     local item="$1"
-    op get account &>/dev/null || eval $(op signin my) | grep -v '^#' >&2 || return 1
-    op get item "$1" | jq '.details.fields[]|select(.designation=="password").value' -r
+    if ! op get account &>/dev/null; then
+        eval $(op signin my) || return 1
+    fi
+    op get item "$1" | jq '.details|select(fields!=null).fields[]|select(.designation=="password").value' -r
     return $?
+}
+
+function opmy() {
+    eval $(op signin my) || return 1
 }
 
