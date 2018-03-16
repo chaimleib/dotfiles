a0fdc2f77c157d503e2af0645f4a40a9227dd903
Chaim Halbert <chalbert@evernote.com>
2018-03-16 12:42:33 -0700

    add cch, cex, chp

--- .bashrc.d/13-project-aliases.sh
+++ .bashrc.d/13-project-aliases.sh
@@ -5,3 +5,8 @@ cdalias cec ~/projects/evernote/marketing-content
 cdalias cece ~/projects/evernote/comm-engine
 cdalias ces ~/projects/evernote/web
 cdalias ceb ~/projects/evernote/blog
+cdalias cex ~/projects/evernote/experiments-service
+
+cdalias chp ~/projects
+cdalias cch ~/Desktop/chaimleib.github.io
+cdalias cch ~/projects/github/chaimleib.github.io
