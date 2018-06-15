087126fa74b5d012bbf88a5d22cd2b9369c6ce9d
Chaim Halbert <chalbert@evernote.com>
2018-06-15 10:58:58 -0700

    simplified pathlist func syntax

--- .bashrc.d/00-pathfuncs.sh
+++ .bashrc.d/00-pathfuncs.sh
@@ -2,29 +2,24 @@
 
 function lspath() {
 # Displays the members of the path variable on separate lines
-  local pathlistname=$1
-  [[ -z $pathlistname ]] && pathlistname="PATH"
-
-  eval "local pathlist=\"\${$pathlistname}\""
-
-  echo $pathlist | tr ':' '\n' | awk 'NF > 0' ||
-    echo "ERROR in lspath(): pathlistname=$pathlistname pathlist=$pathlist" 1>&2
+  local pathlistname="${1:-PATH}"
+  eval "local pathlist=\"\$$pathlistname\""
+  echo "$pathlist" | sed -E -e 's/^:+|:+$//g' -e $'s/:+/\\\n/g'
 }
 
 function _removePath() {
 # if provided, the argument specifies the name of the variable to
 # operate on. Otherwise, it defaults to $PATH.
 # DOES NOT export! echoes the result to stdout.
-  local pathlistname=$2
-  [[ -z $pathlistname ]] && pathlistname="PATH"
+  local pathlistname=${2:-PATH}
 
-  eval "local pathlist=\"\${$pathlistname}\""
+  eval "local pathlist=\"\$$pathlistname\""
 
   result=''
-  for p in `lspath "${pathlistname}" | tr ' ' '\a'`; do
+  for p in `lspath "$pathlistname" | tr ' ' '\a'`; do
     p="`echo "$p" | tr '\a' ' '`"
-    [[ $p != $1 ]] && if [[ -n ${result} ]]; then
-      result="${result}:$p"
+    [[ "$p" != "$1" ]] && if [[ -n "$result" ]]; then
+      result="$result:$p"
     else
       result="$p"
     fi
@@ -37,13 +32,9 @@ function removePath() {
 
 # if provided, the second argument specifies the name of the variable to
 # operate on. Otherwise, it defaults to $PATH.
-  local pathlistname=$2
-  [[ -z $pathlistname ]] && pathlistname="PATH"
-
-  eval "local pathlist=\"\${$pathlistname}\""
-  pathlist="`_removePath "$1" "${pathlistname}"`"
-
-  export $pathlistname="${pathlist}"
+  local pathlistname="${2:-PATH}"
+  local pathlist="`_removePath "$1" "$pathlistname"`"
+  export "$pathlistname"="$pathlist"
 }
 
 function prependPath() {
@@ -52,16 +43,13 @@ function prependPath() {
 # if provided, the second argument specifies the name of the variable to
 # operate on. Otherwise, it defaults to $PATH.
   [[ -d "$1" ]] || return 1
-  local pathlistname=$2
-  [[ -z $pathlistname ]] && pathlistname="PATH"
+  local pathlistname="${2:-PATH}"
+  local pathlist="`_removePath "$1" "$pathlistname"`"
 
-  eval "local pathlist=\"\${$pathlistname}\""
-  pathlist="`_removePath "${1}" "${pathlistname}"`"
-
-  if [[ -z $pathlist ]]; then
-    export $pathlistname="${1}"
+  if [[ -z "$pathlist" ]]; then
+    export "$pathlistname"="$1"
   else
-    export $pathlistname="${1}:${pathlist}"
+    export "$pathlistname"="$1:$pathlist"
   fi
 }
 
@@ -71,16 +59,13 @@ function appendPath() {
 # if provided, the second argument specifies the name of the variable to
 # operate on. Otherwise, it defaults to $PATH.
   [[ -d "$1" ]] || return 1
-  local pathlistname=$2
-  [[ -z $pathlistname ]] && pathlistname="PATH"
+  local pathlistname="${2:-PATH}"
+  local pathlist="`_removePath "$1" "$pathlistname"`"
 
-  eval "local pathlist=\"\${$pathlistname}\""
-  pathlist="`_removePath "${1}" "${pathlistname}"`"
-
-  if [[ -z $pathlist ]]; then
-    export $pathlistname="${1}"
+  if [[ -z "$pathlist" ]]; then
+    export "$pathlistname"="$1"
   else
-    export $pathlistname="${pathlist}:${1}"
+    export "$pathlistname"="$pathlist:$1"
   fi
 }
 
@@ -88,9 +73,9 @@ function appendPath() {
 ##    abspath [path]
 # Converts path to an absolute path. If no path given, gives current directory.
 function abspath() {
-  [[ "$1" != '' ]] && pushd "$1" >/dev/null
+  [[ -n "$1" ]] && pushd "$1" >/dev/null
   pwd
-  [[ "$1" != '' ]] && popd >/dev/null
+  [[ -n "$1" ]] && popd >/dev/null
 }
 
 function relpath() {
