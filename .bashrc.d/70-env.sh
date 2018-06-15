7ae7241cfdf74fc87713a4dd85a662c1f2831223
Chaim Halbert <chalbert@evernote.com>
2018-06-14 17:14:51 -0700

    rm which usage, brew cask defaulted appdir to /Applications ages ago

--- .bashrc.d/70-env.sh
+++ .bashrc.d/70-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export EDITOR="`which vim`"
+export EDITOR="$(command -v vim)"
 export VISUAL="$EDITOR"
 
 # Perl
@@ -8,10 +8,5 @@ if [[ -d "${HOME}/.perl5" ]]; then
     export PERL_MB_OPT="--install_base \"${HOME}/.perl5\""
     export PERL_MM_OPT="INSTALL_BASE=${HOME}/.perl5"
     export PERL5LIB="${HOME}/.perl5/lib/perl5"
-fi
-
-# Homebrew Cask
-if [[ "$OSTYPE" == darwin* ]]; then
-    export HOMEBREW_CASK_OPTS='--appdir=/Applications'
 fi
 
