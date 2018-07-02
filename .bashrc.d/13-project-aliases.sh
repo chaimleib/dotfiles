055b252a1cb2e3cfef2d6ca39cd113a49d0ca830
Chaim Halbert <chalbert@evernote.com>
2018-07-02 15:59:34 -0700

    upd project-aliases

--- .bashrc.d/13-project-aliases.sh
+++ .bashrc.d/13-project-aliases.sh
@@ -6,6 +6,9 @@ cdalias cece ~/projects/evernote/comm-engine
 cdalias ces ~/projects/evernote/web
 cdalias ceb ~/projects/evernote/blog
 cdalias cex ~/projects/evernote/experiments-service
+cdalias cei ~/projects/evernote/ion
+cdalias cer ~/projects/evernote/realtime-server
+cdalias ced ~/projects/evernote/dashboard-microservice
 
 cdalias chp ~/projects
 cdalias cch ~/Desktop/chaimleib.github.io
