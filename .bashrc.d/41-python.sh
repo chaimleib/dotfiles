bb1e671cb75c7f1cb650c0218e22a9baba0798cc
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2018-07-06 08:30:06 -0700

    rm pyenv

--- .bashrc.d/41-python.sh
+++ .bashrc.d/41-python.sh
@@ -2,11 +2,3 @@
 
 [[ -f "$HOME/.pythonrc.py" ]] && export PYTHONSTARTUP="$HOME/.pythonrc.py"
 
-[ -f /usr/local/bin/virtualenvwrapper.sh ] &&
-    . /usr/local/bin/virtualenvwrapper.sh
-if have pyenv; then
-    # https://medium.com/@henriquebastos/the-definitive-guide-to-setup-my-python-workspace-628d68552e14
-    eval "$(pyenv init -)"
-    pyenv virtualenvwrapper_lazy
-fi
-
