50db41085a125182b30be7c5df5152b41c0f97ea
Chaim Halbert <chaim.leib.halbert@gmail.com>
2019-01-23 16:15:58 -0800

    speed improvements to neovim-py3 install, and zap pip upgrade warning

--- mac/installers/300-neovim-python.sh
+++ mac/installers/300-neovim-python.sh
@@ -2,14 +2,10 @@
 set -e
 printf "%-30s %s..." Installing "pyenv-virtualenv for neovim"
 
-function require() {
-  have "$1" && return
-  printf 'Missing: %s\n' "$1"
-  exit 1
-}
+function abort() { echo "$1" >&2; exit 1; }
+function require() { have "$1" || abort "Missing: $1"; }
 
 require nvim
-require python
 require pyenv
 require pyenv-virtualenv
 
@@ -28,19 +24,40 @@ latestPy3=$(pyenv install --list | grep -E '^\s+3[\.0-9]+$' | tail -n1 | xargs)
 echo
 echo Installing python "$latestPy3" with pyenv
 echo n | # don't reinstall if already have latest
-  CONFIGURE_OPTS="--enable-optimizations" pyenv install -v "$latestPy3"
+  # increases install time on pyenv from 2m to 18m, and not much benefit:
+  # CONFIGURE_OPTS="--enable-optimizations"
+  pyenv install -v "$latestPy3"
 
 pyenv virtualenv "$latestPy3" "$ve3" >/dev/null
 pyenv activate "$ve3"
 
-pkg=neovim
+# upgrade pip to silence a warning
 echo
-printf "%-30s %s (in %s)..." "Installing Python package" "$pkg" "$ve3"
-if python -c "import $pkg" &>/dev/null; then
-  echo "Already installed $pkg in $ve3"
-else
-  pip install "$pkg"
+if pip list --outdated | grep pip &>/dev/null; then
+  printf "%-30s %s (in %s)...\n" "Upgrading Python package" "pip" "$ve3"
+  if logs=$(pip install --upgrade pip 2>&1); then
+    echo Done
+  else
+    echo Failed
+    echo "$logs"
+    exit 1
+  fi
 fi
+
+# install neovim python package
+printf "%-30s %s (in %s)...\n" "Installing Python package" neovim "$ve3"
+if python -c "import neovim" &>/dev/null; then
+  echo "Already installed neovim in $ve3"
+else
+  if logs=$(pip install neovim 2>&1); then
+    echo Done
+  else
+    echo Failed
+    echo "$logs"
+    exit 1
+  fi
+fi
+
 pyenv deactivate
 pyenv shell system
 
