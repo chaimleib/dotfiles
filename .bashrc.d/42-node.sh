753e1292e44a6816c1ed932153e5c7a4b91f5f6a
Chaim Halbert <chaim.leib.halbert@gmail.com>
2019-03-13 13:29:25 -0700

    make nvm loader try more paths

--- .bashrc.d/42-node.sh
+++ .bashrc.d/42-node.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# Where to store envs
+NVM_DIR="$HOME/.nvm/nvm.sh"
+export NVM_DIR
+
 # Initialize nvm shell function
-export NVM_DIR="$HOME/.nvm"
-[[ -s $NVM_DIR/nvm.sh ]] && \. "$NVM_DIR/nvm.sh"
+nvm_rcs=(
+  "$HOME/.nvm/nvm.sh"
+  /usr/local/opt/nvm/nvm.sh
+  /usr/local/nvm/nvm.sh
+)
+for rc in "${nvm_rcs[@]}"; do
+  [[ -f "$rc" ]] || continue
+  . "$rc"
+done
 
 # Detect node version from noderc
 function noderc_update() {
