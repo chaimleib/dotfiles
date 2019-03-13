1d412ec818ec46db4aa7b282b1f10131fefc30ab
Chaim Halbert <chaim.leib.halbert@gmail.com>
2019-03-13 13:21:34 -0700

    rm deprecated java flags for tomcat

--- .bashrc.d/61-tomcat.sh
+++ .bashrc.d/61-tomcat.sh
@@ -3,6 +3,6 @@
 TOMCAT_HOME='/usr/local/opt/tomcat/libexec'
 if [[ -d "$TOMCAT_HOME" ]]; then
     export TOMCAT_HOME
-    export JAVA_OPTS="-Xms512m -Xmx1024m -XX:PermSize=512m -XX:MaxPermSize=2048m"
+    export JAVA_OPTS="-Xms512m -Xmx2048m"
 fi
 
