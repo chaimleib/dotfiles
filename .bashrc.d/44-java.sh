7e01f1734c3e227ba374440126f73259217b2b2c
Chaim Halbert <chaim.leib.halbert@gmail.com>
2019-03-28 01:34:09 -0700

    mv JAVA_OPTS from tomcat to java rc

--- .bashrc.d/44-java.sh
+++ .bashrc.d/44-java.sh
@@ -7,4 +7,5 @@ elif [ -d "/usr/local/java-current" ]; then # Linux
     prependPath "$JAVA_HOME"/bin
 fi
 export JAVA_HOME
+export JAVA_OPTS="-Xms512m -Xmx2048m"
 
--- .bashrc.d/61-tomcat.sh
+++ .bashrc.d/61-tomcat.sh
@@ -3,6 +3,5 @@
 TOMCAT_HOME='/usr/local/opt/tomcat/libexec'
 if [[ -d "$TOMCAT_HOME" ]]; then
     export TOMCAT_HOME
-    export JAVA_OPTS="-Xms512m -Xmx2048m"
 fi
 
