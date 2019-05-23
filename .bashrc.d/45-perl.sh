9174d4c604ab406271871f40eff37bc958d82073
Chaim Halbert <chaim.leib.halbert@gmail.com>
2019-05-23 15:27:03 -0700

    split off perl init

---
+++ .bashrc.d/45-perl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -d "${HOME}/.perl5" ]]; then
+    export PERL_MB_OPT="--install_base \"${HOME}/.perl5\""
+    export PERL_MM_OPT="INSTALL_BASE=${HOME}/.perl5"
+    export PERL5LIB="${HOME}/.perl5/lib/perl5"
+fi
+
+
--- .bashrc.d/70-env.sh
+++ .bashrc.d/70-env.sh
@@ -16,10 +16,3 @@ for shell in bash zsh; do
   esac
 done
 
-# Perl
-if [[ -d "${HOME}/.perl5" ]]; then
-    export PERL_MB_OPT="--install_base \"${HOME}/.perl5\""
-    export PERL_MM_OPT="INSTALL_BASE=${HOME}/.perl5"
-    export PERL5LIB="${HOME}/.perl5/lib/perl5"
-fi
-
