aaa360bfe3c27f5ab20c51e84c49a7226cda9efd
Chaim Halbert <chaim.leib.halbert@gmail.com>
2019-05-23 15:31:04 -0700

    set SHELL before anything else happens

--- .bashrc.d/00-env.sh
+++ .bashrc.d/00-env.sh
@@ -4,7 +4,9 @@ export EDITOR='vim'
 export VISUAL="$EDITOR"
 export TIMEFORMAT='%3lR'
 
-# update SHELL to be more accurate
+# By default, SHELL gets set to the user's preferred shell (set via chsh),
+# regardless of the shell currently running. I always want new shells to be
+# like the currently-running shell unless I specify explicitly.
 for shell in bash zsh; do
   case "$0" in
     *$shell)
