43fb0fad602ce53ae5d25fe6737283f1f6573e9c
Chaim Halbert <chaim.leib.halbert@gmail.com>
2019-05-23 15:41:00 -0700

    nvm fixes and zsh support

--- .bashrc.d/42-node.sh
+++ .bashrc.d/42-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Where to store envs
-NVM_DIR="$HOME/.nvm/nvm.sh"
+NVM_DIR="$HOME/.nvm"
 export NVM_DIR
 
 # Initialize nvm shell function
@@ -11,22 +11,23 @@ nvm_rcs=(
   /usr/local/nvm/nvm.sh
 )
 for rc in "${nvm_rcs[@]}"; do
-  [[ -f "$rc" ]] || continue
-  . "$rc"
+  [ -f "$rc" ] || continue
+  source "$rc"
 done
 
 # Detect node version from noderc
 function noderc_update() {
-    if [[ "$PWD" != "$PREV_PWD" ]]; then
-        PREV_PWD="$PWD"
-        if [[ -e ".nvmrc" ]]; then
-            nvm install
-        fi
-    fi
+    [ "$PWD" != "$OLDPWD" ] && [ -f ".nvmrc" ] && nvm install
 }
 
-[[ "$0" == *bash ]] && export -f noderc_update
-
 # Run after every user command
-PROMPT_COMMAND="$PROMPT_COMMAND; noderc_update"
+case "$SHELL" in
+  */bash)
+    export -f noderc_update
+    PROMPT_COMMAND="$PROMPT_COMMAND; noderc_update"
+    ;;
+  */zsh)
+    precmd_functions+=( noderc_update )
+    ;;
+esac
 
