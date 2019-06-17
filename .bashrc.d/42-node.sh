5534fd5d7baa9707bda40069a866e63fd7c8b16b
Chaim Halbert <chaim.leib.halbert@gmail.com>
2019-06-17 16:30:32 -0700

    disable auto nvmrc

--- .bashrc.d/42-node.sh
+++ .bashrc.d/42-node.sh
@@ -7,27 +7,30 @@ export NVM_DIR
 # Initialize nvm shell function
 nvm_rcs=(
   "$HOME/.nvm/nvm.sh"
-  /usr/local/opt/nvm/nvm.sh
   /usr/local/nvm/nvm.sh
+  /usr/local/opt/nvm/nvm.sh
 )
 for rc in "${nvm_rcs[@]}"; do
   [ -f "$rc" ] || continue
   source "$rc"
 done
 
-# Detect node version from noderc
-function noderc_update() {
-    [ "$PWD" != "$OLDPWD" ] && [ -f ".nvmrc" ] && nvm install
-}
+# disabled: load .nvmrc on cd
+if false; then
+  # Detect node version from noderc
+  function noderc_update() {
+      [ "$PWD" != "$OLDPWD" ] && [ -f ".nvmrc" ] && nvm use
+  }
 
-# Run after every user command
-case "$SHELL" in
-  */bash)
-    export -f noderc_update
-    PROMPT_COMMAND="$PROMPT_COMMAND; noderc_update"
-    ;;
-  */zsh)
-    precmd_functions+=( noderc_update )
-    ;;
-esac
+  # Run after every user command
+  case "$SHELL" in
+    */bash)
+      export -f noderc_update
+      PROMPT_COMMAND="$PROMPT_COMMAND; noderc_update"
+      ;;
+    */zsh)
+      precmd_functions+=( noderc_update )
+      ;;
+  esac
+fi
 
