143486cb6285210d911520d775e365ffedf7c216
Chaim Halbert <chaim.leib.halbert@gmail.com>
2019-07-31 13:27:59 -0700

    discovered how to use zsh history vim-style

--- .bashrc.d/86-edit-mode.sh
+++ .bashrc.d/86-edit-mode.sh
@@ -1,6 +1,17 @@
 return
-if [[ -n "$ZSH_VERSION" ]]; then
+if [ -n "$ZSH_VERSION" ]; then
   bindkey -v
-elif [[ -n "$BASH_VERSION" ]]; then
+
+  setopt no_beep
+
+  export HISTFILE="$HOME"/.config/zsh/histfile
+  export HISTSIZE=1000
+  export SAVEHIST=1000
+  setopt extended_history
+  setopt hist_ignore_dups
+  setopt hist_verify
+  setopt share_history
+  setopt hist_find_no_dups
+elif [ -n "$BASH_VERSION" ]; then
   set -o vi
 fi
