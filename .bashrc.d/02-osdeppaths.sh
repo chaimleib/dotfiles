8fdb57d51f82edd93d6c5a47fef739a7d285a60e
Chaim Halbert <chaim.leib.halbert@gmail.com>
2019-09-11 23:34:57 -0700

    osdeppaths style and 0 return

--- .bashrc.d/02-osdeppaths.sh
+++ .bashrc.d/02-osdeppaths.sh
@@ -5,49 +5,51 @@
 case $OSTYPE in
 darwin*)
   # Qt
-  [[ -d /Developer/Qt*/[0-9]*/clang_64/bin ]] &&
-  prependPath /Developer/Qt*/[0-9]*/clang_64/bin
+  if [[ -d /Developer/Qt*/[0-9]*/clang_64/bin ]]; then
+    prependPath /Developer/Qt*/[0-9]*/clang_64/bin
+  fi
   ;;
 *linux*)
   # TeX
   TEX_YEAR=$(
-  ls /usr/local/texlive/ 2>/dev/null |
-  grep -E '^[0-9-]+' |
-  sort -r |
-  head -1
+    ls /usr/local/texlive/ 2>/dev/null |
+      grep -E '^[0-9-]+' |
+      sort -r |
+      head -1
   )
   if [[ -n "$TEX_YEAR" ]]; then
-  # expecting only a directory like x86_64-linux inside bin
-  appendPath /usr/local/texlive/$TEX_YEAR/bin/*
-  appendPath /usr/local/texlive/$TEX_YEAR/texmf-dist/doc/info INFOPATH
-  appendPath /usr/local/texlive/$TEX_YEAR/texmf-dist/doc/man MANPATH
+    # expecting only a directory like x86_64-linux inside bin
+    appendPath /usr/local/texlive/$TEX_YEAR/bin/*
+    appendPath /usr/local/texlive/$TEX_YEAR/texmf-dist/doc/info INFOPATH
+    appendPath /usr/local/texlive/$TEX_YEAR/texmf-dist/doc/man MANPATH
   fi
   ;;
 esac
 
 function _addOPPathTree() {
   bin_dirs=(
-  bin
-  sbin
+    bin
+    sbin
   )
-  if [[ -d "$1" ]]; then
+  if ! [ -d "$1" ]; then
+    return
+  fi
   tree=override
-  [[ -d "$1/$tree" ]] &&
+  [ -d "$1/$tree" ] &&
   for bin in ${bin_dirs[*]}; do
-  prependPath "$1/$tree/$bin"
+    prependPath "$1/$tree/$bin"
   done
 
   tree=provide
-  [[ -d "$1/$tree" ]] &&
+  [ -d "$1/$tree" ] &&
   for bin in ${bin_dirs[*]}; do
-  appendPath "$1/$tree/$bin"
+    appendPath "$1/$tree/$bin"
   done
-  fi
 }
 
-if [[ "$OSTYPE" == darwin* ]]; then
-  _addOPPathTree "${HOME}/local/darwin-`uname -m`"
-fi
+case "$OSTYPE" in
+  darwin*) _addOPPathTree "${HOME}/local/darwin-`uname -m`" ;;
+esac
 _addOPPathTree "${HOME}/local/$OSTYPE-`uname -m`"
 _addOPPathTree "${HOME}/local/all"
-
+return 0
