8061ee351479ce87ad4060341257cf51f238907b
Chaim Halbert <chaim.leib.halbert@gmail.com>
2019-09-11 23:38:25 -0700

    bash completion 0return

--- .bashrc.d/10-bash-completion.sh
+++ .bashrc.d/10-bash-completion.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-[[ -z "$PS1" ]] && return
+[ -z "$PS1" ] && return
 
 if [[ "$0" == *bash ]] && [[ -z "$BASH_COMPLETION_COMPAT_DIR" ]]; then
   f=~/.bash_completion
-  [[ -f "$f" ]] || return
-  [[ "$BASHRC_debug" -ge 2 ]] && echo "Sourcing $f ..."
+  [ -f "$f" ] || return
+  [ "$BASHRC_debug" -ge 2 ] && echo "Sourcing $f ..."
   BASH_COMPLETION="$f"
   BASH_COMPLETION_DIR="${f}.d"
   BASH_COMPLETION_COMPAT_DIR="$BASH_COMPLETION_DIR"
@@ -13,11 +13,16 @@ if [[ "$0" == *bash ]] && [[ -z "$BASH_COMPLETION_COMPAT_DIR" ]]; then
 fi
 
 # Google Cloud SDK
-[[ "$0" == *bash ]] &&
-  [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc' ] &&
-  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
-
-[[ "$0" == *zsh ]] &&
-  [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc' ] &&
-  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+case "$0" in
+*bash)
+  if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc' ]; then
+    source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
+  fi
+  ;;
+*zsh)
+  if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc' ]; then
+    source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+  fi
+  ;;
+esac
 
