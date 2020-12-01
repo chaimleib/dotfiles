7c3f668115ffdc30d5ea597a17414e8624bb7101
Chaim Halbert <chaim.leib.halbert@gmail.com>
2020-12-01 10:55:52 -0700

    disable rvm, use rbenv in avail

--- .bashrc.d/40-ruby.sh
+++ .bashrc.d/40-ruby.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# if have rbenv; then
-#     eval "$(rbenv init -)"
-# fi
+if have rbenv; then
+  eval "$(rbenv init -)"
+fi
 
 # Load RVM into a shell session *as a function*
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+# [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 # Rails
 alias rp='bundle exec rails runner "puts Rails.root"'
