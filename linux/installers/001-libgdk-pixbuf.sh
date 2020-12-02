07053dc4b20fcc19ba9a1a21ed00173f3a8235ce
Chaim Halbert <chaim.leib.halbert@gmail.com>
2020-12-02 13:43:05 -0700

    linux: fix video usermod syntax

--- linux/installers/001-libgdk-pixbuf.sh
+++ linux/installers/001-libgdk-pixbuf.sh
@@ -4,7 +4,7 @@ function do_install() {
   [[ -z "$INSTALL" ]] && echo "INSTALL not set" && return 1
 
   $INSTALL libgdk-pixbuf2.0-0
-  sudo usermod -a -G "$USER" video
+  sudo usermod -a -G video "$USER"
 }
 
 do_install
