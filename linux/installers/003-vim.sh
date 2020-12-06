a1d741672f00966bbf6c654af75cd8c7af10cc32
Chaim Halbert <chaim.leib.halbert@gmail.com>
2020-12-06 02:19:24 -0700

    linux: combine basic tools into single call to apt at start of install

---
+++ linux/installers/003-vim.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function do_install() {
+  [[ -z "$INSTALL" ]] && echo "INSTALL not set" && return 1
+
+  $INSTALL build-essential \
+    w3m w3m-img  \
+    vim \
+    python python3 python3-distutils python-distutils-extra \
+    nodejs npm
+
+  # open browser to add ssh key to github and allow git cloning in later steps
+  w3m https://github.com/settings/ssh/new
+
+  # Install plugins and quit
+  vim -c :PlugUpdate -c :q -c :q
+
+  if ping -c1 raw.githubusercontent.com >/dev/null; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh |
+      bash
+  else
+    echo "$0: raw.githubusercontent.com unreachable" >&2
+    exit 1
+  fi
+}
+
+do_install
+
--- linux/installers/004-rust.sh
+++ linux/installers/004-rust.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 function do_install() {
-  # accept all defaults with yes
-  yes '' |
-    sh <(curl https://sh.rustup.rs -sSf)
+  curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
   ~/.cargo/bin/rustup component add rls rust-analysis rust-src
 }
 
