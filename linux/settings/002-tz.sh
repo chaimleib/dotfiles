e38bc8d139999867391b32440e3db7aa71e2dc65
Chaim Halbert <chaim.leib.halbert@gmail.com>
2020-12-07 22:01:57 -0700

    linux: tz fallback if missing timedatectl

--- linux/settings/002-tz.sh
+++ linux/settings/002-tz.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo timedatectl set-timezone US/Arizona
+if command -v timedatectl >/dev/null; then
+  sudo timedatectl set-timezone US/Arizona
+else
+  sudo ln -sf /usr/share/zoneinfo/US/Arizona /etc/localtime
+fi
