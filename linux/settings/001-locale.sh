d496051c99979da687ad5ebd79552d8461ff952c
Chaim Halbert <chaim.leib.halbert@gmail.com>
2020-12-07 23:09:28 -0700

    linux: fix incomplete sed locale.sh

--- linux/settings/001-locale.sh
+++ linux/settings/001-locale.sh
@@ -2,7 +2,7 @@
 if [ -f /etc/locale.gen ]; then
   # comment out all lines except en_US.UTF-8 UTF-8
   sudo sed -i'' \
-    -e 's/^\([^#]\)/# \1' \
+    -e 's/^\([^#]\)/# \1/' \
     -e 's/# \(en_US\.UTF-8 UTF-8\)/\1/' \
     /etc/locale.gen
   sudo locale-gen
