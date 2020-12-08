bd5697ae1e48addad653d31b8e5963ae708ad479
Chaim Halbert <chaim.leib.halbert@gmail.com>
2020-12-07 20:49:38 -0700

    linux: locale en_US.UTF-8

---
+++ linux/settings/001-locale.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -f /etc/locale.gen ]; then
+  # comment out all lines except en_US.UTF-8 UTF-8
+  sudo sed -i'' \
+    -e 's/^\([^#]\)/# \1' \
+    -e 's/# \(en_US\.UTF-8 UTF-8\)/\1/' \
+    /etc/locale.gen
+  sudo locale-gen
+  sudo update-locale LANG=en_US.UTF-8
+fi
