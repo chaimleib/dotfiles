c6199047ffb3de9e70d16e58890615c47e5ed84c
Chaim Halbert <chaim.leib.halbert@gmail.com>
2021-08-24 22:57:13 -0700

    osdeppaths.sh: avoid calling append/prependPath for speed

--- .bashrc.d/02-osdeppaths.sh
+++ .bashrc.d/02-osdeppaths.sh
@@ -7,16 +7,16 @@ function _addOPPathTree() {
   fi
   tree=override
   [ -d "$1/$tree" ] &&
-    prependPath "$1/$tree/bin"
+    PATH="$1/$tree/bin":"$PATH"
 
   tree=provide
   [ -d "$1/$tree" ] &&
-    appendPath "$1/$tree/bin"
+    PATH="$PATH":"$1/$tree/bin"
 }
 
 case "$OSTYPE" in
   darwin*) _addOPPathTree "${HOME}/local/darwin-`uname -m`" ;;
+  *) _addOPPathTree "${HOME}/local/$OSTYPE-`uname -m`" ;;
 esac
-_addOPPathTree "${HOME}/local/$OSTYPE-`uname -m`"
 _addOPPathTree "${HOME}/local/all"
 return 0
