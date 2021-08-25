4fed5b5c04283f2f7f853e9d1e78302d58f0eed6
Chaim Halbert <chaim.leib.halbert@gmail.com>
2021-08-24 20:46:23 -0700

    fix find openjdk quotes

--- .bashrc.d/01-paths.sh
+++ .bashrc.d/01-paths.sh
@@ -8,7 +8,7 @@ export PYENV_ROOT="$HOME/.pyenv"
 if [ -d "/Library/Java/Home" ]; then # Mac
     JAVA_HOME="/Library/Java/Home"
 elif [ -d /usr/local/Cellar/openjdk@11 ]; then # homebrew
-    JAVA_HOME=$(find “/usr/local/Cellar/openjdk@11” -type d -depth 1|sort|tail -n 1)
+    JAVA_HOME=$(find "/usr/local/Cellar/openjdk@11" -type d -depth 1|sort|tail -n 1)
 elif [ -d "/usr/local/java-current" ]; then # Linux
     JAVA_HOME="/usr/local/java-current"
 fi
