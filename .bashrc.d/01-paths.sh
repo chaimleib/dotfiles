8f50914596d498256f529445f4390dda58e77335
Chaim Halbert <chaim.leib.halbert@gmail.com>
2021-10-14 18:56:04 -0700

    paths: conditionals on mysql@5.7 and java

--- .bashrc.d/01-paths.sh
+++ .bashrc.d/01-paths.sh
@@ -4,6 +4,11 @@ removePath .
 # python
 export PYENV_ROOT="$HOME/.pyenv"
 
+# Override default binaries with usr binaries
+PATH="$PYENV_ROOT"/bin:~/go/bin:/usr/local/bin:/usr/local/sbin:"$PATH"
+
+[ -d /usr/local/opt/mysql@5.7 ] && PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+
 # java
 if [ -d "/Library/Java/Home" ]; then # Mac
     JAVA_HOME="/Library/Java/Home"
@@ -12,13 +17,11 @@ elif [ -d /usr/local/Cellar/openjdk@11 ]; then # homebrew
 elif [ -d "/usr/local/java-current" ]; then # Linux
     JAVA_HOME="/usr/local/java-current"
 fi
-if [ "$JAVA_HOME" ]; then
+if [ -n "$JAVA_HOME" ] && [ -d "$JAVA_HOME" ]; then
   export JAVA_HOME
+  PATH="$JAVA_HOME/bin:$PATH"
 fi
 export JAVA_OPTS="-Xms512m -Xmx4096m"
-
-# Override default binaries with usr binaries
-PATH="$JAVA_HOME"/bin:/usr/local/opt/mysql@5.7/bin:"$PYENV_ROOT"/bin:~/go/bin:/usr/local/bin:/usr/local/sbin:"$PATH"
 
 PKG_CONFIG_PATH="$PKG_CONFIG_PATH":/usr/local/lib:/usr/X11/lib/pkgconfig
 
