9b5a231242124caaee0f4a9ecb385e5aa23717e8
Chaim Halbert <chaim.leib.halbert@gmail.com>
2021-10-21 04:36:55 -0700

    install_linux: try braces instead of parens

--- linux/installers/000-basics.sh
+++ linux/installers/000-basics.sh
@@ -26,12 +26,13 @@ function do_install() {
   [ -z "$INSTALL" ] && echo "INSTALL not set" && return 1
 
   $INSTALL \
-    `is_pacman && echo base-devel ||
-      is_apk && echo build-base ||
-      is_apt && echo build-essential` \
+    `{ is_pacman && echo base-devel } ||
+      { is_apk && echo build-base } ||
+      { is_apt && echo build-essential }` \
       w3m \
-      ` (is_pacman || is_apk) && echo imlib2 ||
-        is_apt && echo w3m-img` \
+      `{ is_pacman && echo imlib2 } ||
+        { is_apk && echo imlib2 } ||
+        { is_apt && echo w3m-img }` \
     neovim \
     tree \
     ripgrep \
@@ -40,12 +41,14 @@ function do_install() {
     bash zsh \
     tmux \
     python3 `is_apt && echo python3-distutils` \
-    `is_pacman && echo python-pip ||
-      is_apk && echo py3-pip` \
+    `{ is_pacman && echo python-pip } ||
+      { is_apk && echo py3-pip }` \
     nodejs npm
 
   # open browser to add ssh key to github and allow git cloning in later steps
-  w3m https://github.com/settings/ssh/new
+  if ! grep -F github.com ~/.ssh/known_hosts &>/dev/null; then
+    w3m https://github.com/settings/ssh/new
+  fi
 
   mv ~/.gitconfig ~/.gitconfig.aside
 
