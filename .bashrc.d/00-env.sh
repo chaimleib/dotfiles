3b36b8261bd5ab3289e76ad4accf1df0d14b56f6
Chaim Halbert <chaim.leib.halbert@gmail.com>
2021-10-31 16:27:44 -0700

    env.sh: determine SHELL with ps instead of $0

--- .bashrc.d/00-env.sh
+++ .bashrc.d/00-env.sh
@@ -8,16 +8,12 @@ export RIPGREP_CONFIG_PATH=~/.config/rg/rgrc
 # By default, SHELL gets set to the user's preferred shell (set via chsh),
 # regardless of the shell currently running. I always want new shells to be
 # like the currently-running shell unless I specify explicitly.
-for shell in bash zsh; do
-  case "$0" in
-    *$shell)
-      case "$SHELL" in
-        */$shell) ;;
-        *) export SHELL=$(which $shell) ;;
-      esac
-      ;;
-  esac
-done
+cm=$(ps -o pid,comm | awk '$1=='$$' { print $2 }')
+case "$cm" in
+  -*) export SHELL=$(which "${cm#-}") ;;
+  *.sh) ;;
+  *sh) export SHELL=$(which "$cm") ;;
+esac
 
 case "$TERM" in
   xterm*)
