65f5c161ebd29ab1a523b51f829561b44884d944
Chaim Halbert <chaim.leib.halbert@gmail.com>
2023-02-17 12:03:16 -0700

    generate ssh keys using ed25519

--- common/settings/ssh.sh
+++ common/settings/ssh.sh
@@ -5,12 +5,12 @@ function setup_ids() {
         mkdir "$HOME"/.ssh
     fi
 
-    local out="$HOME"/.ssh/id_rsa
+    local out="$HOME"/.ssh/id_ed25519
     if [[ -e "$out" && -e "$out".pub ]]; then
         return
     fi
 
-    ssh-keygen -t rsa -C "chaim.leib.halbert@gmail.com" -N '' -f "$out"
+    ssh-keygen -t ed25519 -C "chaim.leib.halbert@gmail.com" -f "$out"
     eval "$(ssh-agent -s)"
     ssh-add "$out"
 }
