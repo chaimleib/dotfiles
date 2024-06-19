7389681d5b9c333a680223e91a61e0f77a76e032
Chaim Halbert <chaim.leib.halbert@gmail.com>
2024-06-19 14:49:11 -0700

    path: rm java, mysql@5.7; add snaps

--- .bashrc.d/01-paths.sh
+++ .bashrc.d/01-paths.sh
@@ -7,21 +7,7 @@ export PYENV_ROOT="$HOME/.pyenv"
 # Override default binaries with usr binaries
 PATH="$PYENV_ROOT"/bin:~/go/bin:~/.yarn/bin:~/.cargo/bin:~/.local/bin:/usr/local/bin:/usr/local/sbin:"$PATH"
 
-[ -d /usr/local/opt/mysql@5.7 ] && PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
-
-# java
-if [ -d "/Library/Java/Home" ]; then # Mac
-    JAVA_HOME="/Library/Java/Home"
-elif [ -d /usr/local/Cellar/openjdk@11 ]; then # homebrew
-    JAVA_HOME=$(find "/usr/local/Cellar/openjdk@11" -type d -depth 1|sort|tail -n 1)
-elif [ -d "/usr/local/java-current" ]; then # Linux
-    JAVA_HOME="/usr/local/java-current"
-fi
-if [ -n "$JAVA_HOME" ] && [ -d "$JAVA_HOME" ]; then
-  export JAVA_HOME
-  PATH="$JAVA_HOME/bin:$PATH"
-fi
-export JAVA_OPTS="-Xms512m -Xmx4096m"
+[ -d /var/lib/snapd/snap/bin ] && PATH="/var/lib/snapd/snap/bin:$PATH"
 
 # for compiling 3DP firmware
 # appendPath ~/.platformio/penv/bin
